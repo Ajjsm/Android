4b3d55fae843154960829ac1323a60f0214c9c1d
Ajjsm <juan.ajjsm@gmail.com>
2016-07-06 21:23:28 +0200

    Primera subida para aplicacion de facturacion para establecimientoss

---
+++ FacturacionTrabajos/app/src/main/java/com/drawer/juanaj/facturaciontrabajos/fragments/ContactarFragment.java
@@ -0,0 +1,24 @@
+package com.drawer.juanaj.facturaciontrabajos.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.drawer.juanaj.facturaciontrabajos.R;
+
+/**
+ * Created by JuanAJ on 06/07/2016.
+ */
+public class ContactarFragment extends Fragment{
+    View rootview;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState){
+        rootview = inflater.inflate(R.layout.contactar_fragment, container, false);
+        return rootview;
+    }
+}
---
+++ FacturacionTrabajos/app/src/main/java/com/drawer/juanaj/facturaciontrabajos/fragments/ListaFragment.java
@@ -0,0 +1,25 @@
+package com.drawer.juanaj.facturaciontrabajos.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.drawer.juanaj.facturaciontrabajos.R;
+
+/**
+ * Created by JuanAJ on 06/07/2016.
+ */
+public class ListaFragment extends Fragment {
+    View rootview;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstanceState){
+        rootview = inflater.inflate(R.layout.lista_fragment, container, false);
+                return rootview;
+    }
+
+}
